8702879a1fa945644b1afce58ec748bdf8db6e69
Atzac <isaacquidutemelo13@gmail.com>
2023-01-30 16:01:26 -0300

    fix: no-undef declarations on .tests

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -3,9 +3,7 @@ import { render } from '@testing-library/react';
 
 import Button from './button';
 
-// eslint-disable-next-line no-undef
 describe('Button', () => {
-  // eslint-disable-next-line no-undef
   test('renders the Button component', () => {
     render(<Button label="Hello world!" />);
   });
