029cce3a447eae936dc875b3b8a4ae32660d7bde
Atzac <72033041+Atzac@users.noreply.github.com>
2023-01-31 09:53:06 -0300

    3 add jest and testing library (#18)
    Co-authored-by: Iury Nogueira <iunog@hotmail.com>
    Co-authored-by: Alysson Keysson <alysson.mascarenhas@grupobrisanet.com.br>

---
+++ src/components/button/button.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Button from './button';
+
+describe('Button', () => {
+  test('renders the Button component', () => {
+    const label = 'Hello world!';
+    render(<Button label={label} />);
+    expect(screen.queryAllByText(label)).toHaveLength(1);
+  });
+});
