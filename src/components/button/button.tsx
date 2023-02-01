2cde48ab84d3f59c7c6b0d961189d4bb0cc51880
Atzac <isaacquidutemelo13@gmail.com>
2023-02-01 13:52:35 -0300

    feat: add and config Stitches #3

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,12 +1,15 @@
 import React, { InputHTMLAttributes } from 'react';
-import './styles.scss';
-
+import { StitcheButton } from './styles';
 export interface ButtonProps extends InputHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
 const Button = ({ label }: ButtonProps) => {
-  return <button className="btn">{label}</button>;
+  return (
+    <>
+      <StitcheButton>{label}</StitcheButton>
+    </>
+  );
 };
 
 export default Button;
