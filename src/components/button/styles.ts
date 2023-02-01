0b42bdcf86058ee3870afc2d5182142ad8343768
Atzac <isaacquidutemelo13@gmail.com>
2023-02-01 16:31:44 -0300

    feat: stitches config and style

---
+++ src/components/button/styles.ts
@@ -0,0 +1,25 @@
+//import { styled } from "@stitches/react";
+import stitches from '../../stitches.config';
+
+const { styled } = stitches;
+
+export const StitcheButton = styled('button', {
+  width: '50%',
+  color: 'White',
+  fontSize: '$rg',
+  fontWeight: 'normal',
+  backgroundColor: '$primaryColor',
+  borderRadius: '6px',
+  padding: '10px 15px',
+  border: 'none',
+  '&:hover': {
+    backgroundColor: '$primary7',
+  },
+  variants: {
+    size: {
+      sm: {
+        width: '75px',
+      },
+    },
+  },
+});
