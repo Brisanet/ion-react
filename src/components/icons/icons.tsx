e17c5090d6ef82201de66af121ccaca850e606ea
Deeved Hiuston <deeved@gmail.com>
2023-02-06 21:27:28 -0300

    chore: adding DOMPurify in icon path

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -1,3 +1,4 @@
+import DOMPurify from 'dompurify';
 import { iconsPaths, iconType } from './svgs/icons';
 
 export type SvgModule = typeof import('./svgs/icons');
@@ -15,22 +16,32 @@ const iconDefault: IonIconProps = {
   size: 24,
 };
 
+function pathPurify(iconType: iconType): string {
+  const iconPath = iconsPaths[iconType];
+  const iconPathPurify = DOMPurify.sanitize(`<svg> ${iconPath} </svg>`);
+  const pathWithoutSvg = iconPathPurify.substring(5, iconPathPurify.length - 6);
+  return pathWithoutSvg;
+}
+
 const IonIcon = ({
   type = iconDefault.type,
   color = iconDefault.color,
   size = iconDefault.size,
 }: IonIconProps) => {
-  return (
+  const iconPath = pathPurify(type);
+  const icon = (
     <svg
       viewBox="0 0 24 24"
-      fill={color}
-      xmlns="http://www.w3.org/2000/svg"
-      width={`${size}px`}
       height={`${size}px`}
-      data-testid={`ion-icon-${type}`}
-      dangerouslySetInnerHTML={{ __html: iconsPaths[type] }}
-    ></svg>
+      width={`${size}px`}
+      fill={color}
+      dangerouslySetInnerHTML={{
+        __html: iconPath,
+      }}
+    />
   );
+
+  return icon;
 };
 
 export default IonIcon;
