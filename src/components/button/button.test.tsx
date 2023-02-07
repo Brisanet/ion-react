5159990bfddc2b93398cfc374a18baa57355b62d
Atzac <isaacquidutemelo13@gmail.com>
2023-02-07 11:36:22 -0300

    test: add class and disabled tests

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Button from './button';
+import { ButtonProps } from './button';
+
+function renderAndGetButton(props: ButtonProps): HTMLElement {
+  render(<Button label={props.label || 'Button'} {...props} />);
+  return screen.getByRole('button', { name: props.label });
+}
 
 describe('Button', () => {
   test('renders the Button component', () => {
@@ -12,12 +19,33 @@ describe('Button', () => {
 
   test('execute the onClick event', () => {
     const handleClick = jest.fn();
-
-    render(<Button type="primary" label="Button" onClick={handleClick} />);
-
-    fireEvent.click(screen.getByRole('button', { name: 'Button' }));
-
-    // expect mock handler to be called
+    const button = renderAndGetButton({
+      type: 'primary',
+      onClick: handleClick,
+    });
+    fireEvent.click(button);
     expect(handleClick).toBeCalledTimes(1);
+  });
+
+  test('is disabled', () => {
+    const button = renderAndGetButton({
+      label: 'Button',
+      type: 'secondary',
+      disabled: true,
+    });
+    expect(button).toHaveAttribute('disabled');
+  });
+
+  const buttonTypes: Array<ButtonProps['type']> = [
+    'primary',
+    'secondary',
+    'ghost',
+    'dashed',
+  ];
+  test.each(buttonTypes)('should render all types', async (type) => {
+    expect(renderAndGetButton({ type })).toHaveAttribute(
+      'class',
+      expect.stringContaining(`type-${type}`)
+    );
   });
 });
