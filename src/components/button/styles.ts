2215ab48b20fe3d7410f9a6b359fd3519e193f87
Atzac <72033041+Atzac@users.noreply.github.com>
2023-02-14 09:04:52 -0300

    Redesign button component #35 (#37) :sparkles:
    Co-authored-by: Iury Nogueira <nogueiraiury20@gmail.com>

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,24 +1,141 @@
 import stitches from '../../stitches.config';
+import { getBgFontBorderColors } from '../utils/getBgFontBorderColors';
 
 const { styled } = stitches;
 
-export const StitcheButton = styled('button', {
-  width: '50%',
-  color: 'White',
-  fontSize: '$rg',
-  fontWeight: 'normal',
-  backgroundColor: '$primaryColor',
-  borderRadius: '6px',
-  padding: '10px 15px',
-  border: 'none',
-  '&:hover': {
-    backgroundColor: '$primary7',
-  },
+// Componente Button
+export const ButtonStyles = styled('button', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '6px 16px',
+  borderRadius: '8px',
+  fontWeight: '600',
+  cursor: 'pointer',
+  minWidth: '32px',
+  height: '32px',
+  gap: '8px',
+
   variants: {
-    size: {
-      sm: {
-        width: '75px',
+    type: {
+      primary: {
+        ...getBgFontBorderColors('$primaryColor', '$neutral1', ''),
+
+        '&:hover': {
+          background: '$primary5',
+        },
+        '&:active': { background: '$primary7' },
+        '&:disabled': {
+          ...getBgFontBorderColors('$neutral2', '$neutral5', ''),
+          cursor: 'not-allowed',
+        },
       },
+      secondary: {
+        ...getBgFontBorderColors('$neutral1', '$primaryColor', '$neutral4'),
+
+        '&:hover': getBgFontBorderColors('$primary1', '$primary5', '$primary4'),
+        '&:active': getBgFontBorderColors(
+          '$primary2',
+          '$primary5',
+          '$primary7'
+        ),
+        '&:disabled': {
+          ...getBgFontBorderColors('$neutral2 ', '$neutral5 ', '$neutral5 '),
+          cursor: 'not-allowed',
+        },
+      },
+      ghost: {
+        ...getBgFontBorderColors('transparent', '$primaryColor', ''),
+
+        '&:hover': {
+          ...getBgFontBorderColors('$primary1', '$primary5', ''),
+        },
+        '&:active': {
+          ...getBgFontBorderColors('$primary2', '$primary7', ''),
+        },
+        '&:disabled': {
+          ...getBgFontBorderColors('transparent', '$neutral5', ''),
+          cursor: 'not-allowed',
+        },
+      },
+      dashed: {
+        ...getBgFontBorderColors('transparent', '$primaryColor', ''),
+        border: '1px dashed $neutral4',
+
+        '&:hover': {
+          ...getBgFontBorderColors('$primary1', '$primary5', ''),
+          borderColor: '$primary4',
+        },
+        '&:active': {
+          ...getBgFontBorderColors('$primary2', '$primary7', ''),
+          borderColor: '$primary5',
+        },
+        '&:disabled': {
+          ...getBgFontBorderColors('$neutral3', '$neutral5', ''),
+          borderColor: '$neutral5',
+          cursor: 'not-allowed',
+        },
+      },
+    },
+
+    danger: {
+      primary: {
+        ...getBgFontBorderColors('$negative6', '$negative1', ''),
+        '&:hover': {
+          ...getBgFontBorderColors('$negative5', '$neutral1', ''),
+        },
+        '&:active': {
+          background: '$negative7',
+        },
+        '&:disabled': {
+          ...getBgFontBorderColors('$neutral2', '$neutral5', ''),
+          cursor: 'not-allowed',
+        },
+      },
+      secondary: {
+        color: '$negativeColor',
+        '&:hover': {
+          ...getBgFontBorderColors('$negative1', '$negative5', '$negative3'),
+        },
+        '&:active': {
+          ...getBgFontBorderColors('$negative2', '$negative7', ''),
+        },
+        '&:disabled': {
+          ...getBgFontBorderColors('$neutral2 ', '$neutral5 ', '$neutral5 '),
+          cursor: 'not-allowed',
+        },
+      },
+      ghost: {
+        color: '$negative6',
+        '&:hover': {
+          ...getBgFontBorderColors('$negative1', '$negative5', ''),
+        },
+        '&:active': {
+          ...getBgFontBorderColors('$negative2', '$negative7', ''),
+        },
+        '&:disabled': {
+          ...getBgFontBorderColors('transparent', '$neutral5', ''),
+          cursor: 'not-allowed',
+        },
+      },
+      dashed: {
+        border: '1px dashed $neutral4',
+        color: '$negative6',
+        '&:hover': {
+          ...getBgFontBorderColors('$negative1', '$negative5', ''),
+          borderColor: '$negative3',
+        },
+        '&:active': {
+          ...getBgFontBorderColors('$negative2', '$negative7', ''),
+          borderColor: '$negative6',
+        },
+        '&:disabled': {
+          ...getBgFontBorderColors('$neutral3', '$neutral5', ''),
+          borderColor: '$neutral5',
+          cursor: 'not-allowed',
+        },
+      },
+      none: {},
     },
   },
 });
