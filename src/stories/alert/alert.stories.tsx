7aa1c342a28fde563507a611e600b0538652001c
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2023-02-14 10:58:24 -0300

    docs: adding more examples

--- src/stories/alert/alert.stories.tsx
+++ src/stories/alert/alert.stories.tsx
@@ -6,7 +6,6 @@ export default {
   title: 'Ion/Feedback/Alert',
   component: IonAlert,
   argTypes: {
-    handClick: { action: 'clicked' },
     type: {
       name: 'type',
       defaultValue: 'success',
@@ -21,9 +20,49 @@ const Template: ComponentStory<typeof IonAlert> = (args: AlertProps) => (
 );
 
 export const AlertDefault = Template.bind({});
+AlertDefault.storyName = 'Alert message';
 AlertDefault.args = {
   message: 'Example of alert message',
+};
+
+export const AlertSuccess = Template.bind({});
+AlertSuccess.storyName = 'Alert success';
+AlertSuccess.args = {
+  message: 'Example of alert message',
   type: 'success',
-  hideBackground: false,
-  closable: false,
+};
+
+export const AlertWarning = Template.bind({});
+AlertWarning.storyName = 'Alert warning';
+AlertWarning.args = {
+  message: 'Example of alert message',
+  type: 'warning',
+};
+
+export const AlertInfo = Template.bind({});
+AlertInfo.storyName = 'Alert info';
+AlertInfo.args = {
+  message: 'Example of alert message',
+  type: 'info',
+};
+
+export const AlertNegative = Template.bind({});
+AlertNegative.storyName = 'Alert negative';
+AlertNegative.args = {
+  message: 'Example of alert message',
+  type: 'negative',
+};
+
+export const AlertClosable = Template.bind({});
+AlertClosable.storyName = 'Alert closable';
+AlertClosable.args = {
+  message: 'Example of alert message',
+  closable: true,
+};
+
+export const AlertWithBackground = Template.bind({});
+AlertWithBackground.storyName = 'Alert with background';
+AlertWithBackground.args = {
+  message: 'Example of alert message',
+  hideBackground: true,
 };
