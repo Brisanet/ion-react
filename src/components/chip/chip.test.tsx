3a22f96eb03bcca8c93447ef4b273ffdda342402
Iury Nogueira <nogueiraiury20@gmail.com>
2023-02-15 15:27:43 -0300

    feat: improve jest-dom imports #76 (#77)

--- src/components/chip/chip.test.tsx
+++ src/components/chip/chip.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import IonChip, { ChipProps } from './chip';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
 import React from 'react';
 
 const clickEvent = jest.fn();
