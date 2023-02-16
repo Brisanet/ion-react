bd087321041a379f0172c419c2a8064becf3d4e9
MarcosOliveir4 <marcosassis071@gmail.com>
2023-02-16 14:09:57 -0300

    test: improving the tests

--- src/components/typhography/heading/heading.spec.tsx
+++ src/components/typhography/heading/heading.spec.tsx
@@ -8,53 +8,68 @@ const sut = (props: IonHeadingProps = defaultProps) => {
   return render(<IonHeading {...props} />);
 };
 
+const headingTypes: Array<IonHeadingProps['type']> = ['h1', 'h2', 'h3', 'h4'];
+const headingSize: Array<IonHeadingProps['size']> = [
+  'medium',
+  'normal',
+  'small',
+];
+const headingcolorScheme: Array<IonHeadingProps['colorScheme']> = [
+  'dark-primary',
+  'dark-secondary',
+  'primary',
+  'secondary',
+];
+const headingFontWeight: Array<IonHeadingProps['weight']> = ['bold', 'medium'];
+const heading: Array<IonHeadingProps> = [
+  { label: 'ion react', type: 'h1' },
+  { label: 'ion react', type: 'h2' },
+  { label: 'ion react', type: 'h3' },
+  { label: 'ion react', type: 'h4' },
+];
+
 describe('<IonHeading />', () => {
-  it('should render the heading with the correct label', () => {
-    sut();
-    expect(
-      screen.getByRole('heading', { name: defaultProps.label })
-    ).toBeInTheDocument();
-  });
+  it.each(heading)(
+    'should render the heading with the correct label $type',
+    (heading) => {
+      sut(heading);
+      expect(
+        screen.getByRole('heading', { name: heading.label })
+      ).toBeInTheDocument();
+    }
+  );
 
-  it('should render the heading with the correct size', () => {
-    sut();
-    expect(screen.getByTestId('ion-heading-h1')).toHaveStyle({
-      'font-size': '2em',
-    });
-    sut({ ...defaultProps, type: 'h2' });
-    expect(screen.getByTestId('ion-heading-h2')).toHaveStyle({
-      'font-size': '1.5em',
-    });
-  });
+  it.each(headingSize)(
+    'should render the heading with the correct size %s',
+    (size) => {
+      const { container } = sut({ ...defaultProps, size });
+      const element = container.firstChild as Element;
+      expect(element.className).toContain(`size-${size}`);
+    }
+  );
 
-  it('should render the heading with the correct color', () => {
-    sut();
-    expect(screen.getByTestId('ion-heading-h1')).toHaveStyle({
-      color: '$neutral8',
-    });
-    sut({ ...defaultProps, type: 'h2', colorScheme: 'secondary' });
-    expect(screen.getByTestId('ion-heading-h1')).toHaveStyle({
-      color: '$neutral7',
-    });
-  });
+  it.each(headingcolorScheme)(
+    'should render the heading with the correct color %s',
+    (colorScheme) => {
+      const { container } = sut({ ...defaultProps, colorScheme });
+      const element = container.firstChild as Element;
+      expect(element.className).toContain(`color-${colorScheme}`);
+    }
+  );
 
-  it('should render the heading with the correct font weight', () => {
-    sut();
-    expect(screen.getByTestId('ion-heading-h1')).toHaveStyle({
-      'font-weight': 'bold',
-    });
-  });
+  it.each(headingFontWeight)(
+    'should render the heading with the correct color %s',
+    (weight) => {
+      const { container } = sut({ ...defaultProps, weight });
+      const element = container.firstChild as Element;
+      expect(element.className).toContain(`weight-${weight}`);
+    }
+  );
 
   describe('snapshot tests', () => {
-    it('should render snapshot headings', () => {
-      sut({ ...defaultProps, type: 'h1' });
-      expect(screen.getByTestId('ion-heading-h1')).toMatchSnapshot();
-      sut({ ...defaultProps, type: 'h2' });
-      expect(screen.getByTestId('ion-heading-h2')).toMatchSnapshot();
-      sut({ ...defaultProps, type: 'h3' });
-      expect(screen.getByTestId('ion-heading-h3')).toMatchSnapshot();
-      sut({ ...defaultProps, type: 'h4' });
-      expect(screen.getByTestId('ion-heading-h4')).toMatchSnapshot();
+    it.each(headingTypes)('should render snapshot heading %s', (type) => {
+      const { container } = sut({ ...defaultProps, type });
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
 });
