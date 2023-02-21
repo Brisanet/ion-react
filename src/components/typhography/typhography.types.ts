93a9f13ef07e6a8e5a15983bc1a37b32e4c00542
MarcosOliveir4 <marcosassis071@gmail.com>
2023-02-21 11:32:49 -0300

    perf: improving IonBody and IonHeading interface types

--- src/components/typhography/typhography.types.ts
+++ src/components/typhography/typhography.types.ts
@@ -1,18 +1,3 @@
-type MyPick<T, S extends keyof T> = {
-  [key in S]: T[key];
-};
-
-interface IonTyphographySize {
-  small: 'small';
-  medium: 'medium';
-  normal: 'normal';
-}
-
-interface IonTyphographyWeight {
-  normal: 'normal';
-  medium: 'medium';
-  bold: 'bold';
-}
 type IonTyphographyStyle = 'normal' | 'italic';
 
 type IonTyphographyColorScheme =
@@ -26,17 +11,20 @@ interface BaseIonTyphography {
   colorScheme?: IonTyphographyColorScheme;
 }
 
-type BodySizes = 'small' | 'medium' | 'normal';
-type RemoveMedium<TType> = TType extends 'medium' ? never : TType;
+type IonTyphographySize = 'small' | 'medium' | 'normal';
+type IonTyphographyWeight = 'normal' | 'medium' | 'bold';
+
+type IonBodySize<TType> = TType extends 'medium' ? never : TType;
+type IonHeadingWeight<TType> = TType extends 'normal' ? never : TType;
 
 export interface IonBodyInterface extends BaseIonTyphography {
-  size?: RemoveMedium<BodySizes>;
-  weight?: keyof MyPick<IonTyphographyWeight, 'normal' | 'medium' | 'bold'>;
+  size?: IonBodySize<IonTyphographySize>;
+  weight?: IonTyphographyWeight;
   style?: IonTyphographyStyle;
 }
 
 export interface IonHeadingInterface extends BaseIonTyphography {
   type: 'h1' | 'h2' | 'h3' | 'h4';
-  size?: keyof MyPick<IonTyphographySize, 'normal' | 'small' | 'medium'>;
-  weight?: keyof MyPick<IonTyphographyWeight, 'medium' | 'bold'>;
+  size?: IonTyphographySize;
+  weight?: IonHeadingWeight<IonTyphographyWeight>;
 }
