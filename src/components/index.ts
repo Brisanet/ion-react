f8cc8b510c3fc39a4899ef3bdeecc352eff199e5
Atzac <isaacquidutemelo13@gmail.com>
2023-03-21 10:36:39 -0300

    fix: add IonBadge export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export { default as Icon } from './icons';
 export { default as IonChip } from './chip';
 export { default as IonTag } from './tag';
 export { default as IonSteps } from './steps';
+export { default as IonBadge } from './badge';
 export * from './typhography';
 export * from './alert';
