b5abf020606b9df428a9edcaeaa21aebf02a8f6e
Deeved Hiuston <deeved@gmail.com>
2023-03-28 14:38:29 -0300

    refactor: standardizing step component export/import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export { default as IonSteps } from './steps';
 export { default as IonBadge } from './badge';
 export * from './alert';
 export * from './button';
 export * from './chip';
 export * from './icons';
+export * from './steps';
 export * from './tag';
 export * from './typhography';
--- src/components/steps/steps.tsx
+++ src/components/steps/steps.tsx
@@ -138,7 +138,7 @@ const validateChangeOfStep = (currentIndex: number, limit: number): number => {
   return currentIndex;
 };
 
-const IonSteps = ({
+export const IonSteps = ({
   current = 1,
   steps,
   clickable = false,
@@ -214,5 +214,3 @@ const IonSteps = ({
     </StepsContainerStyle>
   );
 };
-
-export default IonSteps;
