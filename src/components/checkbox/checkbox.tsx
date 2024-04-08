4db4b37a679a7e7cbc7d2f00b6a4598c34a088bf
danilo-moreira-brisa <danilo.moreira@grupobrisanet.com.br>
2024-04-08 16:48:37 -0300

    refactor: remove unnecessary props

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -1,36 +1,14 @@
-import { InputHTMLAttributes, useState } from 'react';
+import { InputHTMLAttributes } from 'react';
 import { Checkbox, CheckboxContainer, Label } from './styles';
 
 export type CheckboxProps = {
   label?: string;
-  checked?: boolean;
-  disabled?: boolean;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export const IonCheckbox = ({
-  label,
-  checked = false,
-  disabled = false,
-  id,
-  ...props
-}: CheckboxProps) => {
-  const [isChecked, setIsChecked] = useState<boolean>(checked || false);
-
-  const handleChange = () => {
-    setIsChecked(!isChecked);
-  };
-
+export const IonCheckbox = ({ label, id, ...props }: CheckboxProps) => {
   return (
     <CheckboxContainer>
-      <Checkbox
-        {...props}
-        id={id}
-        type='checkbox'
-        data-testid='ion-checkbox'
-        checked={isChecked}
-        disabled={disabled}
-        onChange={handleChange}
-      />
+      <Checkbox {...props} id={id} type='checkbox' data-testid='ion-checkbox' />
       {!!label && <Label htmlFor={id}>{label}</Label>}
     </CheckboxContainer>
   );
