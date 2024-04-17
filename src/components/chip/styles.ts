c99fa4bcbe67ea722eb2a84b8f1838c93fc7eda0
Danilo Alves <138066684+danilo-moreira-brisa@users.noreply.github.com>
2024-04-17 15:03:32 -0300

    refactor: ion chip with styled components (#142)

--- src/components/chip/styles.ts
+++ src/components/chip/styles.ts
@@ -1,103 +1,129 @@
-import stitches from '../../stitches.config';
-import { getFillBgFontBorderColors } from '../utils';
+import { css, DefaultTheme, RuleSet, styled } from 'styled-components';
+import { ChipSizes } from './chip';
 
-const { styled } = stitches;
+type ChipStyleProps = {
+  $size: ChipSizes;
+  $selected: boolean;
+};
 
-export const ChipStyle = styled('button', {
-  border: '1px solid $neutral4',
-  borderRadius: '100px',
-  cursor: 'pointer',
-  outline: 'none',
-  backgroundColor: '$neutral1',
-  color: '$primaryColor',
-  fontWeight: '600',
-  alignItems: 'center',
+const sizes: (theme: DefaultTheme, size: ChipSizes) => RuleSet<object> = (
+  { font },
+  size
+) => {
+  return {
+    sm: css`
+      height: 24px;
+      padding: 4px 8px;
+      ${font.size[12]}
+    `,
+    md: css`
+      height: 32px;
+      padding: 6px 12px;
+      ${font.size[14]}
+    `,
+  }[size];
+};
 
-  div: {
-    display: 'flex',
-    alignItems: 'center',
-    gap: '8px',
+const selectedColors: (theme: DefaultTheme) => RuleSet<object> = ({
+  colors,
+}) => css`
+  background-color: ${colors.primary[1]};
+  border: 1.5px solid ${colors.main.primary};
+  color: ${colors.main.primary};
 
-    svg: {
-      fill: '$primaryColor',
-    },
-  },
+  svg {
+    fill: ${colors.main.primary};
+  }
 
-  '&:hover': {
-    ...getFillBgFontBorderColors({
-      svgFillColor: '$primary5',
-      backgroundColor: '$secondaryColor',
-      fontColor: '$primary5',
-      borderColor: '$primary3',
-    }),
-  },
+  &:hover {
+    background-color: ${colors.primary[2]};
+    border-color: ${colors.primary[5]};
+    color: ${colors.primary[5]};
 
-  '&:active': {
-    outline: 'none',
-    ...getFillBgFontBorderColors({
-      svgFillColor: '$primary7',
-      backgroundColor: '$primary2',
-      fontColor: '$primary7',
-      borderColor: '$primary5',
-    }),
-  },
+    svg {
+      fill: ${colors.primary[5]};
+    }
+  }
 
-  '&:focus-visible': {
-    outline: '2px solid $primary6',
-    outlineOffset: '2px',
-    ...getFillBgFontBorderColors({
-      svgFillColor: '$primary5',
-      backgroundColor: '$secondaryColor',
-      fontColor: '$primary5',
-      borderColor: '$primary3',
-    }),
-  },
+  &:focus-visible {
+    background-color: ${colors.primary[1]};
+    border-color: ${colors.primary[4]};
+    color: ${colors.primary[5]};
 
-  '&:disabled': {
-    outline: 'none',
-    cursor: 'not-allowed',
-    ...getFillBgFontBorderColors({
-      svgFillColor: '$neutral5',
-      backgroundColor: '$neutral3',
-      fontColor: '$neutral5',
-      borderColor: '$neutral5',
-    }),
-  },
+    svg {
+      fill: ${colors.primary[5]};
+    }
+  }
 
-  defaultVariants: {
-    size: 'sm',
-  },
-  variants: {
-    selected: {
-      true: {
-        border: '1.5px solid $primary5',
-        borderRadius: '100px',
-        fontWeight: '600',
-        cursor: 'pointer',
-        backgroundColor: '$primary2',
+  &:active {
+    background-color: ${colors.primary[3]};
+    border-color: ${colors.primary[7]};
+    color: ${colors.primary[7]};
 
-        '&:hover': {
-          border: '1.5px solid $primary5',
-          backgroundColor: '$primary2',
-          color: '$primary5',
+    svg {
+      fill: ${colors.primary[7]};
+    }
+  }
 
-          svg: {
-            fill: '$primary5',
-          },
-        },
-      },
-    },
-    size: {
-      sm: {
-        padding: '4px 8px',
-        lineHeight: '16px',
-        fontSize: '12px',
-      },
-      md: {
-        padding: '6px 12px',
-        lineHeight: '20px',
-        fontSize: '14px',
-      },
-    },
-  },
-});
+  &:disabled {
+    background-color: ${colors.neutral[3]};
+    border-color: ${colors.neutral[5]};
+    color: ${colors.neutral[5]};
+
+    svg {
+      fill: ${colors.neutral[5]};
+    }
+  }
+`;
+
+export const Chip = styled.button<ChipStyleProps>`
+  ${({ theme, $size, $selected }) => css`
+    background-color: ${theme.colors.neutral[1]};
+    border: 1px solid ${theme.colors.neutral[4]};
+    border-radius: 100px;
+    color: ${theme.colors.main.primary};
+    cursor: pointer;
+    font-weight: 600;
+    ${theme.utils.flex.center(8)}
+    ${theme.utils.focus}
+    ${sizes(theme, $size)}
+    
+    svg {
+      fill: ${theme.colors.main.primary};
+    }
+
+    &:hover,
+    &:focus-visible {
+      background-color: ${theme.colors.primary[1]};
+      border-color: ${theme.colors.primary[3]};
+      color: ${theme.colors.primary[5]};
+
+      svg {
+        fill: ${theme.colors.primary[5]};
+      }
+    }
+
+    &:active {
+      background-color: ${theme.colors.primary[2]};
+      border-color: ${theme.colors.primary[5]};
+      color: ${theme.colors.primary[7]};
+
+      svg {
+        fill: ${theme.colors.primary[7]};
+      }
+    }
+
+    &:disabled {
+      cursor: not-allowed;
+      background-color: ${theme.colors.neutral[3]};
+      border-color: ${theme.colors.neutral[5]};
+      color: ${theme.colors.neutral[5]};
+
+      svg {
+        fill: ${theme.colors.neutral[5]};
+      }
+    }
+
+    ${$selected && selectedColors(theme)}
+  `}
+`;
