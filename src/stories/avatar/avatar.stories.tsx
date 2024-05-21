8e83646bcfb3c156866a9ed82f90e89e8a93cb60
allan-chagas-brisa <allan.chagas@grupobrisanet.com.br>
2024-05-21 12:55:00 -0300

    fix: avatar stories

--- src/stories/avatar/avatar.stories.tsx
+++ src/stories/avatar/avatar.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { AvatarType, IonAvatar } from '../../components/avatar';
+import { IonAvatar } from '../../components/avatar';
 
 export default {
   title: 'Ion/Data Display/Avatar',
@@ -18,12 +18,12 @@ Default.args = {
 
 export const Icon = Template.bind({});
 Icon.args = {
-  type: AvatarType.icon,
+  type: 'icon',
 };
 
 export const Photo = Template.bind({});
 Photo.args = {
-  type: AvatarType.photo,
+  type: 'photo',
   value: 'ana souza',
   image:
     'https://64.media.tumblr.com/40e2174ab5e68b1eabbc3dfc78607cef/c1effc67d5c3a1fd-20/s540x810/9d6ce72fcddf97841e7410a0652dd9d5f018b35d.pnj',
